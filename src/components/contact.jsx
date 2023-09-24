f0b0493d4deeb9b21aa1dfb2278573e9e7257714
Marialdinha <marialda@bellsouth.net>
2023-09-24 12:59:44 -0500

    Adde contact

---
+++ src/components/contact.jsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+
+    return(
+
+        <section id="contact-me" className="content_section">
+        <h2 className="content_h2"> Contact Me </h2>
+        <div className="content_h2_div">
+            <nav>
+                <ul>
+                    <li><a href="tel:+1954-464-7795">+1 954-464-7795</a>
+                    </li>
+                    <li><a href="mailto:maiada@bellsouth.net">marialda@bellsouth.net</a>
+                    </li>
+                    <li><a href="https://github.com/Marialdinha/Portfolio-Page" > GitHub</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+    );
+}
